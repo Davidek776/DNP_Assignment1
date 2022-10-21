a80ad636e8aa478504707a1554399ac0f7a8e599
Davidek776 <kocurdavid776@gmail.com>
2022-10-21 17:56:29 +0200

    beggining2

---
+++ Solution1/Domain/DTOs/UserCreationDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain.DTOs;
+
+public class UserCreationDto
+{
+    public string UserName { get;}
+    public string password { get;}
+
+    public UserCreationDto(string userName,string password)
+    {
+        UserName = userName;
+        this.password = password;
+    }
+}
---
+++ Solution1/Domain/Models/Post.cs
@@ -0,0 +1,16 @@
+﻿namespace Domain.Models;
+
+public class Post
+{
+    public int Id { get; set; }
+    public string title { get; }
+    public string body { get; }
+    public User Owner { get; }
+
+    public Post(User owner, string title, string body)
+    {
+        this.Owner = owner;
+        this.title = title;
+        this.body = body;
+    }
+}
